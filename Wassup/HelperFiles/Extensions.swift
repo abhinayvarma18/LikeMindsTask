ec78ff97c0ab7b6a523306bc3ee5ecf9930dbf84
Abhinay Varma <abhinaycpian@gmail.com>
2020-07-28 11:10:41 +0530

    Project Restructured

--- Wassup/HelperFiles/Extensions.swift
+++ Wassup/HelperFiles/Extensions.swift
@@ -10,11 +10,9 @@ import Foundation
 
 extension Date {
     func toMillis() -> Int64! {
-        return Int64(self.timeIntervalSince1970 * 1000)
+           return Int64(self.timeIntervalSince1970 * 1000)
     }
-}
-
-extension Date {
+    
     func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
         return calendar.dateComponents(Set(components), from: self)
     }
